252e34bd90e3f127aaf9d292f277af9cb6f179e8
Marilyn Marquez <marilynmarquez@MMarquez.local>
2022-12-17 13:26:20 -0600

    basic functions

--- main.py
+++ main.py
@@ -6,6 +6,19 @@ screen = Screen()
 def move_forward():
     tim.forward(10)
 
+def move_backwards():
+    tim.back(10)
+
+def counter_clockwise():
+    tim.left(10)
+
+def clockwise():
+    tim.right(10)
+
+def clear_screen():
+    tim.reset()
+
+
 
 
 
@@ -14,5 +27,11 @@ def move_forward():
 #tell screen to start listening:
 screen.listen()
 #bind keystroke to event (use event listener onkey(), need to create function to pass to this):
-screen.onkey(key = "space", fun = move_forward)
+screen.onkey(key = "w", fun = move_forward)
+screen.onkey(key = "s", fun = move_backwards)
+screen.onkey(key = "a", fun = counter_clockwise)
+screen.onkey(key = "d", fun = clockwise)
+screen.onkey(key = "c", fun = clear_screen)
+
+
 screen.exitonclick()
